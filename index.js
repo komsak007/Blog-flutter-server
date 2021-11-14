7213aa9387f798a204a72bb320be4d81ec9d0821
komsak007 <lert_zodiac@hotmail.com>
2021-11-14 22:34:55 +0700

    Add Model profile

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(express.json());
 const userRoute = require("./routes/user");
 app.use("/user", userRoute);
 
+const profileRoute = require("./routes/profile");
+app.use("/profile", profileRoute);
+
 app.route("/").get((req, res) => res.json({ rest: "your first rest api 11" }));
 
 app.listen(Port, () => console.log(`Your server in running on port ${Port}`));
