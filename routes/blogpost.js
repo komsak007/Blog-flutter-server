b20a2b8f286b8645972efbb991d1bc37f48d49b2
unknown <lert_zodiac@hotmail.com>
2021-11-25 16:08:00 +0700

    get username for show on drawer

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -2,6 +2,42 @@ const express = require("express");
 const router = express.Router();
 const BlogPost = require("../models/blogpost.model");
 const middleWare = require("../middleware");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, res, cb) => {
+    cb(null, req.params.id + ".jpg");
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 6 },
+});
+
+router.patch(
+  "/add/coverImage/:id",
+  middleWare.checkToken,
+  upload.single("img"),
+  (req, res) => {
+    BlogPost.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          coverImage: req.file.path,
+        },
+      },
+      { new: true },
+      (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+      }
+    );
+  }
+);
 
 router.post("/Add", middleWare.checkToken, (req, res) => {
   const blogpost = new BlogPost({
@@ -19,6 +55,34 @@ router.post("/Add", middleWare.checkToken, (req, res) => {
       res.json({ err: err });
       console.log(err);
     });
+});
+
+router.get("/getOwnBlog", middleWare.checkToken, (req, res) => {
+  BlogPost.find({ username: req.decoded.username }, (err, result) => {
+    if (err) return res.json(err);
+    return res.json({ data: result });
+  });
+});
+
+router.get("/getOtherBlog", middleWare.checkToken, (req, res) => {
+  BlogPost.find({ username: { $ne: req.decoded.username } }, (err, result) => {
+    if (err) return res.json(err);
+    return res.json({ data: result });
+  });
+});
+
+router.delete("/delete/:id", middleWare.checkToken, (req, res) => {
+  BlogPost.findOneAndDelete(
+    { $and: [{ username: req.decoded.username }, { _id: req.params.id }] },
+    (err, result) => {
+      if (err) return res.json(err);
+      else if (result) {
+        console.log(result);
+        return res.json("Blog deleted");
+      }
+      return res.json("Blog not deleted");
+    }
+  );
 });
 
 module.exports = router;
